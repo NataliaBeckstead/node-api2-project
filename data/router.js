f4b5380116d516b38acbc8740f7dbc5e8f6fa872
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-07 15:21:20 -0600

    get by id and comments

--- data/router.js
+++ data/router.js
@@ -16,4 +16,40 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+        Posts.findById(req.params.id)
+        .then((post) => {
+			if (post.length) {
+				res.status(200).json(post);
+			} else {
+				res
+					.status(404)
+					.json({ message: "The post with the specified ID does not exist." });
+			}
+		})
+		.catch((err) =>
+			res
+				.status(500)
+				.json({ error: "The post information could not be retrieved" })
+		);
+});
+
+router.get("/:id/comments", (req, res) => {
+	Posts.findPostComments(req.params.id)
+		.then((comments) => {
+			if (comments.length) {
+				res.status(200).json(comments);
+			} else {
+				res
+					.status(404)
+					.json({ message: "The comments does not exist." });
+			}
+		})
+		.catch((err) =>
+			res
+				.status(500)
+				.json({ error: "The comments information could not be retrieved." })
+		);
+});
+
 module.exports = router;
